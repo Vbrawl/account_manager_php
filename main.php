0dccc4b7a04eeac17260599dc69fcc569522b072
Vbrawl <greninjagr@gmail.com>
2023-08-28 18:12:17 +0300

    Added import file that loads the whole plugin.

---
+++ main.php
@@ -0,0 +1,9 @@
+<?php
+
+
+require_once('relocation.php');
+
+require_once(DATABASE_ADAPTER_PATH.'/main.php');
+require_once(ACCOUNT_MANAGER_PATH.'/src/database.php');
+require_once(ACCOUNT_MANAGER_PATH.'/src/account.php');
+require_once(ACCOUNT_MANAGER_PATH.'/src/permission.php');
